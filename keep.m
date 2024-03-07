3a2c518dece1f78ad47858a7242835032efbdd38
Vitollee <134598037+Vitollee@users.noreply.github.com>
2024-03-08 00:56:20 +0800

    hold test

---
+++ keep.m
@@ -0,0 +1,22 @@
+clc
+clear
+
+z = readmatrix('with.csv','Range','A1:A8002');
+z_ref = readmatrix('with.csv' ,'Range','F1:F8002');
+z1=readmatrix('with.csv','Range','G1:G8002');
+z2=readmatrix('without.csv','Range','G1:G8002');
+
+figure; 
+hold on; 
+
+plot(z, z_ref , 'r--');
+plot(z, z1 , 'b'); 
+plot(z, z2 , 'g'); 
+
+
+legend('REF','with','without'); 
+xlabel('time'); 
+ylabel('Z Value'); 
+title('Vicon hold test'); 
+
+hold off; 
